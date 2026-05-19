4df7f27cd02843c20090b5cfe0962a0e696f1654
Tomoro Saito <s.tomoro0726@gmail.com>
2026-05-19 14:33:23 +0900

    APIを発見

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "https://api.plateauview.mlit.go.jp/datacatalog/plateau-datasets";
+
+    // HTTP GET
+    let response = reqwest::get(url).await?;
+
+    // バイト列取得
+    let bytes = response.bytes().await?;
+
+    // ファイル保存
+    let mut file = File::create("file.json").await?;
+    file.write_all(&bytes).await?;
+
+    println!("download complete");
+
+    Ok(())
 }
